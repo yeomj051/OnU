2df99a202c5f0f1b01bea8cc7e91f11352779eb4
한상원 <swhan0428@gmail.com>
2023-04-27 15:34:15 +0900

    [S08P31A703-86] FE/Feat: Add twin.macro styled-component
    - twin.macro 및 styled.component 설치

---
+++ frontend/.babelrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    ['next/babel', { 'preset-react': { runtime: 'automatic' } }],
+  ],
+  plugins: [
+    'babel-plugin-macros',
+    ['babel-plugin-styled-components', { ssr: true }],
+  ],
+};
