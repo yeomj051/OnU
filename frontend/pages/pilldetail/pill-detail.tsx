a85720d86b1c02ccb12aa949a51f88b52388c000
ljh991004 <ljh991004@naver.com>
2023-04-27 17:58:22 +0900

    [S08P31A703-76] FE/Rename : add PillDetail folder

---
+++ frontend/pages/pilldetail/pill-detail.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PillDetailMain from '@/components/PillDetail/PillDetailMain';
+
+function PillDetail() {
+  return <PillDetailMain />;
+}
+
+export default PillDetail;
