e7f740dcca665682e5a1dfb8eff2248f894158ad
조우진 <sky5962146@gmail.com>
2023-04-27 17:45:12 +0900

    [S08P31A703-110] BE/Feat: Add sending sms function

--- backend/src/main/java/com/ssafy/onu/controller/UserController.java
+++ backend/src/main/java/com/ssafy/onu/controller/UserController.java
@@ -1,7 +1,40 @@
 package com.ssafy.onu.controller;
 
+import com.ssafy.onu.service.SmsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
+    private static final String MESSAGE = "message";
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    private SmsService smsService;
+
+    public UserController(SmsService smsService) {
+        this.smsService = smsService;
+    }
+
+    @GetMapping("/phone")
+    public ResponseEntity<Map<String, Object>> sendSms(@RequestParam String phone) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        if(smsService.sendMessage(phone) != null) {
+            resultMap.put(MESSAGE, SUCCESS);
+            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+        } else {
+            resultMap.put(MESSAGE, FAIL);
+            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
