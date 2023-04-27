be559140849bb608ae6c4f2dde0efb2df040c4e9
한상원 <swhan0428@gmail.com>
2023-04-27 13:50:22 +0900

    [S08P31A703-86] FE/Feat: Add react-query setting
    - react-query 초기설정

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import Head from 'next/head';
+import '@/styles/globals.css';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, //window가 focus되었을 경우 자동으로 refetch해주는 옵션
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={client}>
+      <Head>
+        <title>OnU: my Own NUtrients</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
