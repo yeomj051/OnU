1a04f44b2b1c2befa8d5c7b70fb7080c5ade8e73
ljh991004 <ljh991004@naver.com>
2023-05-01 17:53:02 +0900

    [S08P31A703-150] FE/Feat : make compare page's box components

--- frontend/containers/PillComparePage/PillCompareMain.tsx
+++ frontend/containers/PillComparePage/PillCompareMain.tsx
@@ -2,63 +2,123 @@ import React from 'react';
 import { useState } from 'react';
 import PillImg from '../../public/jong_pill.svg';
 import { Heart, HeartFill } from '@emotion-icons/bootstrap';
+import PillCompareBadge from './PillCompareBadge';
+import PillCompareImg from './PillCompareImg';
 
 type Props = {};
 
 function PillCompareMain({}: Props) {
-  const [like, setLike] = useState<boolean>(true);
+  const [fisrtLike, setFirstLike] = useState<boolean>(true);
+  const [secondLike, setSecondLike] = useState<boolean>(true);
+  const [firstList, setFirstList] = useState<string[]>([
+    'Haccp',
+    'GMP',
+  ]);
+  const [secondList, setSecondList] = useState<string[]>(['Haccp']);
+
   //좋아요 on/off
-  const likeOrNot = () => {
-    setLike(!like);
+  const fisrtLikeOrNot = () => {
+    setFirstLike(!fisrtLike);
+  };
+
+  const secondLikeOrNot = () => {
+    setSecondLike(!secondLike);
   };
 
   return (
     <div className="h-[100vh]">
-      <div className="mx-4">
-        <PillImg className="mx-auto my-6 w-1/2" />
+      <div className="mx-4 grid grid-cols-2">
+        <div className="col-span-1 pr-2">
+          <PillImg className="mx-auto my-6 w-1/2" />
 
-        <div>
-          <div className="grid grid-cols-12">
-            <div className="col-span-8 text-sm font-light text-gray-500 grid row-span-3 content-center ml-1">
-              종근당
+          <div>
+            <div className="grid grid-cols-4">
+              <div className="col-span-3 text-sm font-light text-gray-500 grid row-span-3 content-center ml-1">
+                종근당
+              </div>
+
+              <div className="col-span-1 grid justify-items-end content-center">
+                <button onClick={fisrtLikeOrNot}>
+                  {fisrtLike ? (
+                    <HeartFill className="w-8 h-8 text-red-500 pr-4" />
+                  ) : (
+                    <Heart className="w-8 h-8 text-red-500 pr-4" />
+                  )}
+                </button>
+              </div>
             </div>
-            <div className="col-span-3 grid justify-items-end row-span-3 content-center">
-              <button className="badge badge-outline text-gray-500 w-24 h-6">
-                비교하기
-              </button>
-            </div>
-            <div className="col-span-1 grid justify-items-end content-center">
-              <button onClick={likeOrNot}>
-                {like ? (
-                  <HeartFill className="w-8 h-8 text-red-500 pr-4" />
-                ) : (
-                  <Heart className="w-8 h-8 text-red-500 pr-4" />
-                )}
-              </button>
+
+            <div className="bg-yellow-200 text-xl font-medium mt-2">
+              비타민B 군 비타민비 고농축 고함량 활성 수용성
             </div>
           </div>
+        </div>
+        <div className="col-span-1 pl-2">
+          <PillImg className="mx-auto my-6 w-1/2" />
 
-          <div className="bg-yellow-200 text-xl font-medium mt-2">
-            비타민B 군 비타민비 고농축 고함량 활성 수용성
-          </div>
-          <div className=" mt-5 shadow-md">
-            영양제 설명설명설명
-            <br />
-            건강에 좋고 어쩌구 저쩌구
-            <br />
-            몸도 튼튼 마음도 튼튼
+          <div>
+            <div className="grid grid-cols-4">
+              <div className="col-span-3 text-sm font-light text-gray-500 grid row-span-3 content-center ml-1">
+                종근당
+              </div>
+
+              <div className="col-span-1 grid justify-items-end content-center">
+                <button onClick={secondLikeOrNot}>
+                  {secondLike ? (
+                    <HeartFill className="w-8 h-8 text-red-500 pr-4" />
+                  ) : (
+                    <Heart className="w-8 h-8 text-red-500 pr-4" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <div className="bg-yellow-200 text-xl font-medium mt-2">
+              비타민B 군 비타민비 고농축 고함량 활성 수용성
+            </div>
           </div>
         </div>
       </div>
       {/*여기는 파란부분 */}
       <div className="bg-[#F2F9FF] mt-8 h-full pt-6">
-        <hr className="mx-4" />
-        <div className="tabs grid grid-cols-2 justify-items-center py-4 bg-white mx-4">
-          <div className="col-span-1"></div>
-          <div className="col-span-1"></div>
+        {/* <hr className="mx-4" />
+        <div className="tabs grid justify-items-center py-4 bg-white mx-4">
+          <div className="">인증 정보</div>
         </div>
         <hr className="mx-4" />
-        <div className="mx-4"></div>
+        <div className="mx-4">
+          <div className="grid grid-cols-2 py-2">
+            <div className="bg-white col-span-1 border rounded-lg h-20 mr-2"></div>
+            <div className="bg-white col-span-1 border  rounded-lg h-20 ml-2"></div>
+          </div>
+        </div> */}
+        <PillCompareBadge
+          subject="인증정보"
+          first={firstList}
+          second={secondList}
+        />
+        <PillCompareBadge
+          subject="기능성"
+          first={firstList}
+          second={secondList}
+        />
+        <PillCompareImg
+          subject="제형"
+          first={firstList}
+          second={secondList}
+        />
+        <hr className="mx-4" />
+        <div className="tabs grid justify-items-center py-4 bg-white mx-4">
+          <div className="">영양 성분</div>
+        </div>
+        <hr className="mx-4" />
+        <div className="mx-4">
+          <div className="grid grid-cols-7 py-2 bg-white">
+            <div className="col-span-3">뭐야?</div>
+            <div className="col-span-1">비타민A</div>
+            <div className="col-span-3">뭐야?</div>
+          </div>
+        </div>
       </div>
     </div>
   );
