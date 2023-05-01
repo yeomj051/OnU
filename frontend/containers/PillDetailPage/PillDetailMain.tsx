eefb7628ee0b5ad8f63877af6c9055884329741f
ljh991004 <ljh991004@naver.com>
2023-05-01 11:22:50 +0900

    [S08P31A703-149] FE/Feat : Detail css

--- frontend/containers/PillDetailPage/PillDetailMain.tsx
+++ frontend/containers/PillDetailPage/PillDetailMain.tsx
@@ -1,136 +1,92 @@
 import React from 'react';
-import Image from 'next/image';
-import OnULogo from '../../public/OnULogo.png';
+import { useState } from 'react';
+import PillImg from '../../public/jong_pill.svg';
+import { Heart, HeartFill } from '@emotion-icons/bootstrap';
+import PillDetailInfo from './PillDetailInfo';
+import PillDetailReview from './PillDetailReview';
+import tw from 'twin.macro';
 
 type Props = {};
 
+const ButtonToChange = tw.button`
+w-full font-semibold
+
+`;
+
 function PillDetailMain({}: Props): React.ReactElement {
-  const logoString = OnULogo.toString();
-  const logoStringPlusSlash = '/' + logoString;
+  const [infoSwitch, setInfoSwitch] = useState<boolean>(true);
+  const [like, setLike] = useState<boolean>(true);
+
+  //제품상세정보<->리뷰
+  const switchInfo = () => {
+    setInfoSwitch(true);
+  };
+
+  const switchReview = () => {
+    setInfoSwitch(false);
+  };
+
+  //좋아요 on/off
+  const likeOrNot = () => {
+    setLike(!like);
+  };
+
   return (
     <div className="h-[100vh]">
       <div className="mx-4">
-        <Image
-          src={logoStringPlusSlash}
-          alt="pill_picture"
-          width={100}
-          height={50}
-        />
+        <PillImg className="mx-auto my-6 w-1/2" />
 
         <div>
-          <div className="bg-green-200 grid grid-cols-10">
-            <div className="bg-gray-200 col-span-7">종근당</div>
-            <div className="bg-red-200 col-span-2 grid justify-items-end row-span-3 content-center">
-              <button className="badge badge-outline text-gray-500 w-24">
+          <div className="grid grid-cols-12">
+            <div className="col-span-8 text-sm font-light text-gray-500 grid row-span-3 content-center ml-1">
+              종근당
+            </div>
+            <div className="col-span-3 grid justify-items-end row-span-3 content-center">
+              <button className="badge badge-outline text-gray-500 w-24 h-6">
                 비교하기
               </button>
             </div>
-            <div className="bg-red-400  col-span-1 grid justify-items-end content-center">
-              <div>dd</div>
+            <div className="col-span-1 grid justify-items-end content-center">
+              <button onClick={likeOrNot}>
+                {like ? (
+                  <HeartFill className="w-8 h-8 text-red-500 pr-4" />
+                ) : (
+                  <Heart className="w-8 h-8 text-red-500 pr-4" />
+                )}
+              </button>
             </div>
           </div>
 
-          <div className="bg-yellow-200">
+          <div className="bg-yellow-200 text-xl font-medium mt-2">
             비타민B 군 비타민비 고농축 고함량 활성 수용성
           </div>
-          <div className="bg-yellow-100">영양제 설명설명설명</div>
+          <div className=" mt-5 shadow-md">
+            영양제 설명설명설명
+            <br />
+            건강에 좋고 어쩌구 저쩌구
+            <br />
+            몸도 튼튼 마음도 튼튼
+          </div>
         </div>
       </div>
       {/*여기는 파란부분 */}
-      <div className="bg-[#F2F9FF] mt-10 h-full">
-        <div className="tabs grid grid-cols-2 justify-items-center bg-red-200">
+      <div className="bg-[#F2F9FF] mt-8 h-full pt-6">
+        <hr />
+        <div className="tabs grid grid-cols-2 justify-items-center py-4 bg-white">
           <div className="col-span-1">
-            <div className="w-full border-b-2 border-blue-400">
+            <ButtonToChange onClick={switchInfo}>
               제품상세정보
-            </div>
+            </ButtonToChange>
           </div>
-          <div className="col-span-1 border-b-2 border-blue-500">
-            <div className="col-span-1 ">리뷰</div>
+          <div className="col-span-1">
+            <ButtonToChange onClick={switchReview}>
+              리뷰
+            </ButtonToChange>
           </div>
         </div>
-
+        <hr />
         <div className="mx-4">
-          <div className="bg-white min-h-[60px] px-5 py-30 grid grid-cols-6 rounded-lg mt-3">
-            <div className="col-span-1 grid content-center">
-              인증정보
-            </div>
-            <div className="col-span-5 bg-red-50 grid content-center">
-              <div className="badge badge-outline bg-[#90B5EA] ">
-                GMP
-              </div>
-            </div>
-          </div>
-          <div className="bg-white min-h-[60px] px-5 py-30 grid grid-cols-6 rounded-lg mt-3">
-            <div className="col-span-1 grid content-center">
-              기능성
-            </div>
-            <div className="col-span-5 bg-red-50 grid content-center">
-              <div className="badge badge-outline ">GMP</div>
-            </div>
-          </div>
-          <div className="bg-white min-h-[60px] px-5 py-30 grid grid-cols-6 rounded-lg mt-3">
-            <div className="col-span-1 grid content-center">
-              제 형
-            </div>
-            <div className="col-span-5 bg-red-50 grid content-center">
-              <div className="badge badge-outline ">GMP</div>
-            </div>
-          </div>
-          <div className="bg-white min-h-[60px] px-5 py-30 grid grid-cols-6 rounded-lg mt-3">
-            <div className="col-span-1 grid content-center">
-              영양성분
-            </div>
-            <div className="col-span-5 bg-red-50 grid content-center mt-5 mb-3">
-              <div className="grid grid-cols-12">
-                <div className="col-span-2">비타민 A</div>
-                <progress
-                  className="progress progress-error col-span-10 w-full my-3"
-                  value="0"
-                  max="100"
-                ></progress>
-              </div>
-              <div className="grid grid-cols-12">
-                <div className="col-span-2">비타민 A</div>
-                <progress
-                  className="progress progress-error col-span-10 w-full my-3"
-                  value="10"
-                  max="100"
-                ></progress>
-              </div>
-              <div className="grid grid-cols-12">
-                <div className="col-span-2">비타민 A</div>
-                <progress
-                  className="progress progress-error col-span-10 w-full my-3"
-                  value="40"
-                  max="100"
-                ></progress>
-              </div>
-              <div className="grid grid-cols-12">
-                <div className="col-span-2">비타민 A</div>
-                <progress
-                  className="progress progress-error col-span-10 w-full my-3"
-                  value="70"
-                  max="100"
-                ></progress>
-              </div>
-            </div>
-          </div>
-          <div className="bg-white min-h-[60px] px-5 py-30 grid grid-cols-6 rounded-lg mt-3">
-            <div className="col-span-1 grid content-center">
-              섭취시
-              <br />
-              주의사항
-            </div>
-            <div className="col-span-5 bg-red-50 grid content-center">
-              <div className="">카페인과 함께 섭취하지 않는다.</div>
-            </div>
-          </div>
-          <div className="bg-white min-h-[60px] px-5 py-30 grid grid-cols-6 rounded-lg mt-3">
-            <div className="col-span-1 grid content-center">원료</div>
-            <div className="col-span-5 bg-red-50 grid content-center">
-              <div className="badge badge-outline ">GMP</div>
-            </div>
-          </div>
+          {infoSwitch ? <PillDetailInfo /> : <PillDetailReview />}
         </div>
       </div>
     </div>
---
+++ frontend/containers/PillDetailPage/PillDetailReview.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+function PillDetailReview({}: Props) {
+  return <div>PillDetailReview</div>;
+}
+
+export default PillDetailReview;
