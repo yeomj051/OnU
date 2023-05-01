51799f632fb79a4f448e065ac94089a696f59d06
nyh365 <12nyk34@naver.com>
2023-05-01 17:33:13 +0900

    [S08P31A703-183] BE/Feat: Add check nickname Function

--- backend/src/main/java/com/ssafy/onu/controller/UserController.java
+++ backend/src/main/java/com/ssafy/onu/controller/UserController.java
@@ -1,7 +1,55 @@
 package com.ssafy.onu.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.onu.dto.request.ReqUserInfoDto;
+import com.ssafy.onu.dto.response.ResponseUserInfoDto;
+import com.ssafy.onu.service.TokenProviderService;
+import com.ssafy.onu.service.UserService;
+import com.ssafy.onu.util.TokenUtils;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private static final String MESSAGE = "message";
+    private static final String RESULT = "result";
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    private final RedisTemplate redisTemplate;
+    private final UserService userService;
+    private final TokenProviderService tokenProviderService;
+
+    @ApiOperation(value = "닉네임 중복 검사", notes = "회원의 닉네임의 중복 여부를 검사합나다.", response = Map.class)
+    @GetMapping("/{userNickname}")
+    public ResponseEntity<Map<String,Object>> nicknameDpcn(@ApiParam(value = "닉네임", required = true, example = "홍길동")
+                                                           @PathVariable String userNickname){
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+
+        if(userService.nicknameDpcn(userNickname)){
+            resultMap.put(MESSAGE , SUCCESS);
+            status = HttpStatus.OK;
+        } else {
+            resultMap.put(MESSAGE, FAIL);
+            status = HttpStatus.BAD_REQUEST;
+        }
+
+        return new ResponseEntity<>(resultMap, status);
+    }
+
 }
--- backend/src/main/java/com/ssafy/onu/service/UserService.java
+++ backend/src/main/java/com/ssafy/onu/service/UserService.java
@@ -1,7 +1,24 @@
 package com.ssafy.onu.service;
 
+import com.ssafy.onu.dto.request.ReqUserInfoDto;
+import com.ssafy.onu.dto.response.ResponseUserInfoDto;
+import com.ssafy.onu.entity.User;
+import com.ssafy.onu.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    public Boolean nicknameDpcn(String userNickname) {
+        Optional<User> user = userRepository.findByUserNickname(userNickname);
+        if(user.isPresent()) return false;
+        else return true;
+    }
+
+
 }
