810d422264fa225ef8b127084732961feb16c155
한상원 <swhan0428@gmail.com>
2023-05-03 17:02:04 +0900

    [S08P31A703-176] FE/Fix: Refactor files
    - pages 폴더 내 파일은 로직을 드러내지 않도록 변경

--- frontend/pages/user/phoneauth.tsx
+++ frontend/pages/user/phoneauth.tsx
@@ -2,188 +2,18 @@ import React from 'react';
 import { NextPageWithLayout } from '../_app';
 import AppLayout from '@/components/layout/AppLayout';
 import HeaderLayout from '@/components/layout/HeaderLayout';
-import api from '@/apis/config';
-import Timer from '@/components/common/Timer';
-import { useRouter } from 'next/router';
+import PhoneAuth from '@/containers/UserPage/PhoneAuth';
 
-const PhoneAuth: NextPageWithLayout = () => {
-  const [phoneNumber, setPhoneNumber] = React.useState<string>(''); //전화번호
-  const [authCode, setAuthCode] = React.useState<string>(''); //인증번호
-  const [userId, setUserId] = React.useState<number>(0);
-  const [isMessageSent, setIsMessageSent] =
-    React.useState<boolean>(false); //인증 메시지 전송 여부
-  const [time, setTime] = React.useState<number>(180);
-  const [isAuth, setIsAuth] = React.useState<number>(0);
-
-  const router = useRouter();
-
-  React.useEffect(() => {
-    const authNum: string | null = localStorage.getItem(
-      'isPhoneAuthenticated',
-    );
-
-    if (authNum) {
-      setIsAuth(Number.parseInt(authNum));
-      if (Number.parseInt(authNum) <= 2) setIsMessageSent(true);
-      else if (Number.parseInt(authNum) > 2) {
-        alert('인증횟수를 초과했습니다. 다시 시도해주세요');
-        localStorage.removeItem('isPhoneAuthenticated');
-        router.reload();
-      }
-    }
-  }, []);
-
-  //인증시간 만료 로직처리
-  const handleTimeOut = () => {
-    if (confirm('인증시간이 초과되었습니다. 다시 시도해주세요')) {
-      localStorage.removeItem('isAuth');
-      router.reload();
-    }
-  };
-
-  // userId 가져오기
-  //숫자만 입력받도록 정규식 제한
-  const handleInput = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ): void => {
-    e.target.value = e.target.value
-      .replace(/[^0-9.]/g, '')
-      .replace(/(\..*)\./g, '$1');
-  };
-
-  const verifyPhoneNumber = (): void => {
-    // api
-    //   .verifyPhoneNumber(userId, phoneNumber)
-    //   .then((res: any): void => {
-    //     if (res.data.message === 'true') {
-    //       alert('인증번호가 전송되었습니다.');
-    //       setIsMessageSent(true);
-    //     } else {
-    //       throw new Error();
-    //     }
-    //   })
-    //   .catch((err: Error): void => {
-    //     alert('알 수 없는 오류가 발생했습니다. 다시 시도해주세요.');
-    //   });
-    alert('인증번호가 전송되었습니다.');
-    setPhoneNumber('');
-    setAuthCode('');
-    setIsMessageSent(true);
-  };
-
-  const verifyAuthNumber = (): void => {
-    // api
-    //   .verifyPhoneNumber()
-    //   .then((res: any): void => {
-    //     if (res.data.message === 'true') {
-    //       alert('인증이 완료되었습니다.');
-    //       setIsMessageSent(false);
-    //     } else {
-    //       throw new Error();
-    //     }
-    //   })
-    //   .catch((err: Error): void => {
-    //     alert('알 수 없는 오류가 발생했습니다. 다시 시도해주세요.');
-    //   });
-    alert('인증이 완료되었습니다.');
-    localStorage.removeItem('isPhoneAuthenticated');
-    setPhoneNumber('');
-    setAuthCode('');
-    setIsMessageSent(false);
-
-    //다음 페이지로 이동
-  };
-
-  const reVerify = (): void => {
-    localStorage.setItem('isPhoneAuthenticated', String(isAuth + 1));
-    router.reload();
-  };
-
-  return (
-    <div className="grid grid-rows h-[100vh] bg-white pt-20">
-      <div className="grid grid-cols-4">
-        {isMessageSent ? (
-          <div className="col-start-2 col-end-4 space-y-2">
-            <span className="text-xl font-extrabold">
-              전화번호 인증
-            </span>
-            <br />
-            <span className="text-sm text-gray-400 whitespace-nowrap">
-              전송된 인증번호를 입력해주세요
-            </span>
-            <div className="pt-4">
-              <label className="text-base font-bold label-text">
-                인증번호
-              </label>
-              <div className="flex items-center space-x-2">
-                <input
-                  type="text"
-                  className="w-full h-8 max-w-xs py-1 transition duration-300 ease-in bg-transparent border-none underline-input focus:outline-none"
-                  onInput={handleInput}
-                  value={authCode}
-                  onChange={(
-                    e: React.ChangeEvent<HTMLInputElement>,
-                  ): void => setAuthCode(e.target.value)}
-                />
-                <Timer seconds={time} onTimeOut={handleTimeOut} />
-                <button
-                  className="btn btn-xs bg-[#90B5EA] border-none"
-                  onClick={reVerify}
-                >
-                  재전송
-                </button>
-              </div>
-              <hr />
-            </div>
-            <button
-              className="bg-[#90B5EA] text-base border-none btn btn-sm btn-wide"
-              onClick={verifyAuthNumber}
-            >
-              인증 완료
-            </button>
-          </div>
-        ) : (
-          <div className="col-start-2 col-end-4 space-y-2">
-            <span className="text-xl font-extrabold">
-              전화번호 인증
-            </span>
-            <br />
-            <span className="text-sm text-gray-400 whitespace-nowrap">
-              본인확인을 위해 전화번호를 입력해주세요
-            </span>
-            <div className="pt-4">
-              <label className="text-base font-bold label-text">
-                휴대전화 번호
-              </label>
-              <input
-                type="text"
-                className="w-full h-8 max-w-xs py-1 transition duration-300 ease-in bg-transparent border-none underline-input focus:outline-none"
-                value={phoneNumber}
-                onInput={handleInput}
-                onChange={(
-                  e: React.ChangeEvent<HTMLInputElement>,
-                ): void => setPhoneNumber(e.target.value)}
-              ></input>
-              <hr />
-            </div>
-
-            <button
-              className="bg-[#90B5EA] text-base border-none btn btn-sm btn-wide"
-              onClick={verifyPhoneNumber}
-            >
-              인증번호 받기
-            </button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+const PhoneAuthPage: NextPageWithLayout = () => {
+  return <PhoneAuth />;
 };
 
 //레이아웃 적용이 필요하면 getLayout을 호출해주고,
 //AppLayout안에 원하는 레이아웃을 중첩시킬 수도 있습니다
 //추가로 중첩 안시키면 기본 AppLayout이 적용됨
-PhoneAuth.getLayout = function getLayout(page: React.ReactElement) {
+PhoneAuthPage.getLayout = function getLayout(
+  page: React.ReactElement,
+) {
   return (
     <AppLayout>
       <HeaderLayout>{page}</HeaderLayout>
@@ -191,4 +21,4 @@ PhoneAuth.getLayout = function getLayout(page: React.ReactElement) {
   );
 };
 
-export default PhoneAuth;
+export default PhoneAuthPage;
--- frontend/pages/user/signup.tsx
+++ frontend/pages/user/signup.tsx
@@ -1,161 +1,19 @@
 //'use client'
 
-import React, { useEffect } from 'react';
-import Head from 'next/head';
+import React from 'react';
 import { NextPageWithLayout } from '../_app';
 import AppLayout from '@/components/layout/AppLayout';
 import HeaderLayout from '@/components/layout/HeaderLayout';
-import TextInput from '@/components/common/TextInput';
-import Main from '/public/main.svg';
-import api from '@/apis/config';
+import SignUp from '@/containers/UserPage/SignUp';
 
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { TextField } from '@mui/material';
-
-const SignUp: NextPageWithLayout = () => {
-  const [nicknameLabel, setNicknameLabel] = React.useState('');
-  const [nickname, setNickname] = React.useState('');
-  const [randomNumber, setRandomNumber] = React.useState(
-    Math.floor(Math.random() * 100),
-  );
-  const [birth, setBirth] = React.useState<Date | null>();
-  const [gender, setGender] = React.useState('');
-
-  useEffect((): void => {
-    //추후 서버에서 닉네임 중복확인 api를 받아와서 사용
-    // api.checkNickname(nickname).then((res) => {
-    //   if (res.code === 200) {
-    //     setNicknameLabel('사용 가능한 닉네임입니다.');
-    //   } else {
-    //     setNicknameLabel('사용 불가능한 닉네임입니다');
-    //   }
-    // });
-    if (nickname.length >= 2 && nickname.length <= 15) {
-      setNicknameLabel('사용 가능한 닉네임입니다');
-    } else if (nickname.length > 15) {
-      setNicknameLabel('사용 불가능한 닉네임입니다');
-    } else {
-      setNicknameLabel('');
-    }
-    // setNicknameLabel('사용 불가능한 닉네임입니다');
-  }, [nickname]);
-
-  //타이핑시 닉네임 중복확인
-  const checkNickname = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ): void => {
-    e.preventDefault();
-    setNickname(e.target.value);
-  };
-
-  //회원가입 완료처리
-  const registerUser = () => {
-    const data = {
-      nickname,
-      birth,
-      gender,
-    };
-
-    // 회원정보 보내고
-    // api.updateUserInfo(data).then((res) => {});
-
-    // 다음페이지로 이동
-  };
-
-  return (
-    <div className="grid grid-rows h-[100vh] bg-white pt-20">
-      {/* 로고화면 
-        로고가 너무 커서 임시로 주석처리함
-      */}
-      {/* <div className="w-full h-96 bg-[#D8EDFF] flex justify-center items-center pt-10 pl-10">
-        <Main />
-      </div> */}
-      {/* 폼화면 */}
-      <div className="grid grid-cols-4">
-        <div className="col-start-2 col-end-4">
-          <span className="ml-1 text-xl font-extrabold">
-            추가정보 입력
-          </span>
-          <TextInput
-            labelTopL="닉네임"
-            onChange={checkNickname}
-            labelBotR={nicknameLabel}
-            value={nickname}
-          />
-          {nicknameLabel === '사용 불가능한 닉네임입니다' &&
-          nickname.length >= 2 &&
-          nickname.length <= 15 ? (
-            <div className="rounded bg-[#F7F8FA] flex flex-row items-center text-sm space-x-4 m-0 p-2">
-              <div>
-                <span className="text-[#54C6F2] font-bold">
-                  '{nickname + randomNumber}'{' '}
-                </span>
-                <br />
-                <span>이 별명은 어떠신가요? </span>
-              </div>
-              <button
-                className="bg-[#90B5EA] border-none btn btn-sm"
-                onClick={() => setNickname(nickname + randomNumber)}
-              >
-                사용하기
-              </button>
-            </div>
-          ) : null}
-          <div className="flex flex-col mb-4 space-y-2">
-            <label className="pl-1 text-base font-bold label-text">
-              생년월일
-            </label>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                value={birth}
-                format="YYYY-MM-DD"
-                onChange={(newValue) => {
-                  setBirth(newValue);
-                }}
-              />
-            </LocalizationProvider>
-          </div>
-
-          <div className="flex justify-between mb-4">
-            <span className="pl-1 font-bold">성별</span>
-            <div className="flex space-x-2">
-              <div className="flex flex-row space-x-1">
-                <input
-                  type="radio"
-                  name="radio-2"
-                  className="radio radio-info"
-                  checked
-                />
-                <span>남성</span>
-              </div>
-              <div className="flex flex-row space-x-1">
-                <input
-                  type="radio"
-                  name="radio-2"
-                  className="radio radio-info"
-                />
-                <span>여성</span>
-              </div>
-            </div>
-          </div>
-          <button
-            className="bg-[#90B5EA] text-lg border-none btn btn-wide"
-            onClick={registerUser}
-          >
-            회원가입 완료
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+const SignUpPage: NextPageWithLayout = () => {
+  return <SignUp />;
 };
 
 //레이아웃 적용이 필요하면 getLayout을 호출해주고,
 //AppLayout안에 원하는 레이아웃을 중첩시킬 수도 있습니다
 //추가로 중첩 안시키면 기본 AppLayout이 적용됨
-SignUp.getLayout = function getLayout(page: React.ReactElement) {
+SignUpPage.getLayout = function getLayout(page: React.ReactElement) {
   return (
     <AppLayout>
       <HeaderLayout>{page}</HeaderLayout>
@@ -163,4 +21,4 @@ SignUp.getLayout = function getLayout(page: React.ReactElement) {
   );
 };
 
-export default SignUp;
+export default SignUpPage;
