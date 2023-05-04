1a9bee33b1f0840ae6dd257ffd9db0f7d2d9b60d
한상원 <swhan0428@gmail.com>
2023-05-04 10:32:44 +0900

    [S08P31A703-173] FE/Design: Fix Font config
    - Pretendard 가변 폰트 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Pretendard Variable', 'Pretendard', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial':
           'radial-gradient(var(--tw-gradient-stops))',
