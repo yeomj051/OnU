5e10ba60ff77fe6f53c1231d4deeb095a7280194
SJJ <jeenju1995@gmail.com>
2023-05-04 17:32:40 +0900

    [S08P31A703-147] BE/Feat : Add read user calendar date function

--- backend/src/main/java/com/ssafy/onu/controller/MypageController.java
+++ backend/src/main/java/com/ssafy/onu/controller/MypageController.java
@@ -2,6 +2,7 @@ package com.ssafy.onu.controller;
 
 import com.ssafy.onu.dto.*;
 import com.ssafy.onu.dto.request.ReqUserInfoDto;
+import com.ssafy.onu.dto.response.ResponseTakingDateDto;
 import com.ssafy.onu.dto.response.ResponseUserInfoDto;
 import com.ssafy.onu.entity.*;
 import com.ssafy.onu.service.MypageService;
@@ -15,11 +16,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/mypage")
+@RequestMapping("api/mypage")
 @RequiredArgsConstructor
 public class MypageController {
 
@@ -71,6 +73,31 @@ public class MypageController {
         if(afterUpdateUserInfo != null) {
             resultMap.put(MESSAGE, SUCCESS);
             resultMap.put("afterUpdateUserInfo", afterUpdateUserInfo);
+            return new ResponseEntity<>(resultMap, HttpStatus.OK);
+        } else {
+            resultMap.put(MESSAGE, FAIL);
+            return new ResponseEntity<>(resultMap, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @ApiOperation(value = "복용 날짜 조회", notes = "현재 로그인한 사용자의 영양제 복용 날짜를 조회한다.", response = Map.class)
+    @GetMapping("/{userId}/calender")
+    public ResponseEntity<Map<String, Object>> getCalenderDate(@PathVariable @ApiParam(value = "회원 아이디", required = true, example = "0") int userId, Principal principal,
+                                                               @ApiParam(value = "연도 + 월 정보", required = true, example = "0") @RequestParam String date) {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+
+        // PathVariable로 받은 userId와 토큰에 있는 userId 비교
+        if(TokenUtils.compareUserIdAndToken(userId, principal,resultMap)) {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(resultMap, status);
+        }
+
+        List<String> responseTakingDateDto = mypageService.getCheckedDate(userId, date);
+
+        if (responseTakingDateDto != null) {
+            resultMap.put(MESSAGE, SUCCESS);
+            resultMap.put("checkedDate", responseTakingDateDto);
             return new ResponseEntity<>(resultMap, HttpStatus.OK);
         } else {
             resultMap.put(MESSAGE, FAIL);
--- backend/src/main/java/com/ssafy/onu/service/MypageService.java
+++ backend/src/main/java/com/ssafy/onu/service/MypageService.java
@@ -1,14 +1,19 @@
 package com.ssafy.onu.service;
 
 import com.ssafy.onu.dto.request.ReqUserInfoDto;
+import com.ssafy.onu.dto.response.ResponseTakingDateDto;
 import com.ssafy.onu.dto.response.ResponseUserInfoDto;
 import com.ssafy.onu.entity.*;
+import com.ssafy.onu.repository.TakingDateRepository;
 import com.ssafy.onu.repository.UserRepository;
 import lombok.*;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -34,5 +39,14 @@ public class MypageService {
         } else {
             return null;
         }
+    }
+
+    private final TakingDateRepository takingDateRepository;
+
+    // 복용 날짜 조회
+    public List<String> getCheckedDate(int userId, String date) {
+        List<String> checkedDate = takingDateRepository.findByUserId_UserIdAndTakingDateDateContains(userId, date).stream()
+                .map(d -> d.getTakingDateDate()).collect(Collectors.toList());
+        return checkedDate;
     }
 }
