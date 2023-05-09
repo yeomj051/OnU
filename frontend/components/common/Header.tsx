bcdde78681246ed28856baea8008edee07dd3345
한상원 <swhan0428@gmail.com>
2023-05-09 15:37:01 +0900

    [S08P31A703-220] FE/Fix: Fix Error

--- frontend/components/common/Header.tsx
+++ frontend/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import Logo from 'public/logo.svg';
