4e67f55af9b12b9c1ace9dd0b81b01cf85750ba9
ljh991004 <ljh991004@naver.com>
2023-05-09 17:54:53 +0900

    [S08P31A703-218] FE/Feat : create haveStore

--- frontend/store/haveStore.tsx
+++ frontend/store/haveStore.tsx
@@ -1,1 +1,37 @@
 import { create } from 'zustand';
+
+type Have = {
+  nutrientId: number;
+  nutrientName: string;
+  nutrientImageUrl: string;
+  nutrientBrand: string;
+};
+
+type HaveState = {
+  HaveList: Array<Have>;
+
+  setHaves: (newHave: Have) => void;
+  removeHave: (nutrientId: number) => void;
+  resetHaves: () => void;
+};
+
+export const HaveStore = create<HaveState>((set) => ({
+  HaveList: [],
+
+  setHaves: (newHave: Have): void =>
+    set((state: HaveState): { HaveList: Array<Have> } => ({
+      HaveList: [...state.HaveList, newHave],
+    })),
+
+  removeHave: (HaveId: number): void =>
+    set((state: HaveState): { HaveList: Array<Have> } => ({
+      HaveList: state.HaveList.filter(
+        (Have: Have) => Have.nutrientId !== HaveId,
+      ),
+    })),
+
+  resetHaves: (): void =>
+    set(() => ({
+      HaveList: [],
+    })),
+}));
