bfdde593c70a145d841b6111ad1d95b00e993b9f
한상원 <swhan0428@gmail.com>
2023-05-10 17:08:01 +0900

    [S08P31A703-237] FE/Feat: Add Header Feature
    - @mui/icons-material: ^5.11.16 의존성 설치
    - 헤더 조정

--- frontend/components/common/Header.tsx
+++ frontend/components/common/Header.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useRef } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+
 import Logo from 'public/logo.svg';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import HomeIcon from '@mui/icons-material/Home';
+import LoginIcon from '@mui/icons-material/Login';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = (): React.ReactElement => {
   const [isClicked, setIsClicked] = useState(true);
@@ -58,11 +63,6 @@ const Header = (): React.ReactElement => {
     }
   };
 
-  const handleAlarm = () => {
-    if (window.confirm('복용 알림을 설정하시겠습니까?'))
-      router.push('/user/phoneauth');
-  };
-
   const startSearch = () => {
     setIsClicked(!isClicked);
   };
@@ -70,35 +70,12 @@ const Header = (): React.ReactElement => {
   return (
     <div className="fixed top-0 navbar bg-base-100 w-[512px] z-50">
       <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link href="/">홈으로</Link>
-            </li>
-            <li>
-              <Link href="/mypage">프로필</Link>
-            </li>
-          </ul>
-        </div>
+        <button
+          className="px-2 btn btn-ghost btn-sm"
+          onClick={() => router.back()}
+        >
+          <ChevronLeftIcon />
+        </button>
       </div>
 
       {isClicked ? (
@@ -237,11 +214,8 @@ const Header = (): React.ReactElement => {
         </div>
       )}
 
-      <button
-        className="btn btn-ghost btn-circle"
-        onClick={handleAlarm}
-      >
-        <div className="indicator">
+      <div className="dropdown">
+        <label tabIndex={0} className="btn btn-ghost btn-circle">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-5 h-5"
@@ -253,12 +227,34 @@ const Header = (): React.ReactElement => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              d="M4 6h16M4 12h16M4 18h7"
             />
           </svg>
-          <span className="badge badge-xs badge-primary indicator-item"></span>
-        </div>
-      </button>
+        </label>
+        <ul
+          tabIndex={0}
+          className="w-40 p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box"
+        >
+          <li>
+            <Link href="/">
+              <HomeIcon />
+              홈으로
+            </Link>
+          </li>
+          <li>
+            <Link href="/user/login">
+              <LoginIcon />
+              로그인
+            </Link>
+          </li>
+          <li>
+            <Link href="/mypage">
+              <AccountCircleIcon />
+              마이페이지
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
