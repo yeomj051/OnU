5d7aa72c00cf167a099bf15071073879222f84e9
nyh365 <12nyk34@naver.com>
2023-05-11 12:26:41 +0900

    [S08P31A703-182] BE:Fix : edit mapping path

--- backend/src/main/java/com/ssafy/onu/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/onu/controller/AuthController.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private static final String MESSAGE = "message";
