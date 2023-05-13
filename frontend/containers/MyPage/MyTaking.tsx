c94dd6aecbed252916a98f6c73d328b4c9639650
한상원 <swhan0428@gmail.com>
2023-05-14 00:14:01 +0900

    [S08P31A703-222] FE/Feat: Add taking nutrient list feature
    - 복용중 영양제 리스트 조회 기능 추가
    - 복용중 영양제가 없을 때 화면 필요
    - 복용중 영양제 추가/삭제 기능 필요

--- frontend/containers/MyPage/MyTaking.tsx
+++ frontend/containers/MyPage/MyTaking.tsx
@@ -1,15 +1,36 @@
 import { itemDataList } from '@/apis/data';
 import ItemList from '../../components/list/BaseItemList';
+import { useEffect, useState } from 'react';
+import api from '@/apis/config';
 
 //나의 복용중인 영양제 리스트
 const Taking = (): React.ReactElement => {
+  const [userId, setUserId] = useState<number>(0);
+  const [itemData, setItemData] = useState<Item[]>([]);
+
+  useEffect(() => {
+    setUserId(
+      Number.parseInt(localStorage.getItem('userId') as string),
+    );
+  }, []);
+
+  useEffect(() => {
+    getItemData();
+  }, [userId]);
+
+  const getItemData = async () => {
+    return await api.getTakingPillList(userId).then((res) => {
+      setItemData(res.data.takingNutrientList);
+    });
+  };
+
   return (
     <div>
       <p className="ml-2 text-xl font-extrabold text-[#1E266E] mb-2">
         복용 중인 영양제
       </p>
       <div className="flex flex-col items-center w-[400px] bg-white shadow-lg text-xs font-base text-[#909090] rounded-md p-4">
-        <ItemList itemList={itemDataList} />
+        <ItemList itemList={itemData} />
       </div>
     </div>
   );
