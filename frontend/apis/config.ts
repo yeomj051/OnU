07c7a32b2b5f0ccb65ae4c69a72d5cf05bcd27a8
ljh991004 <ljh991004@naver.com>
2023-05-16 10:07:19 +0900

    [S08P31A703-274] FE:FIX : fix interested error

--- frontend/apis/config.ts
+++ frontend/apis/config.ts
@@ -282,8 +282,8 @@ const api = {
   },
 
   //영양제 상세정보 조회
-  async getPillDetail(nutrientId: number) {
-    return await baseAPI(`/nutrient/${nutrientId}?userId=0`, {
+  async getPillDetail(nutrientId: number, userId: number) {
+    return await authAPI(`/nutrient/${nutrientId}?userId=${userId}`, {
       method: 'GET',
     });
   },
