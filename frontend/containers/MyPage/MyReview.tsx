782c4cfd5ad49d320443cad67c763da99c7a4136
한상원 <swhan0428@gmail.com>
2023-05-17 10:42:16 +0900

    [S08P31A703-322] FE/Feat: Add fix/delete review feature

--- frontend/containers/MyPage/MyReview.tsx
+++ frontend/containers/MyPage/MyReview.tsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react';
 import api from '@/apis/config';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import StarRating from '@/components/common/StarRating';
-import { AxiosResponse } from 'axios';
+
+import MyReviewItem from './MyReviewItem';
 
 //내가 쓴 리뷰 리스트
 const Review = (): React.ReactElement => {
   const [userId, setUserId] = useState<number>();
   const [itemData, setItemData] = useState<Review[]>([]);
-  const router = useRouter();
 
   useEffect(() => {
     setUserId(
@@ -19,53 +16,14 @@ const Review = (): React.ReactElement => {
 
   useEffect(() => {
     if (userId !== null && userId !== undefined) {
-      getItemData(userId).then((res: AxiosResponse) => {
-        // API 응답의 데이터 구조에 대한 안전한 처리를 추가합니다.
-        if (res?.data?.myReviewList) {
-          setItemData(res.data.myReviewList);
-        }
-        console.log(res);
-      });
+      fetchReviews(userId);
     }
   }, [userId]);
 
-  const getItemData = async (id: number): Promise<AxiosResponse> => {
-    return await api.getReviewList(id);
-  };
-
-  const updateReview = (
-    reviewId: number,
-    reviewContent: string,
-    reviewScore: number,
-  ): void => {
-    if (window.confirm('리뷰를 수정하시겠습니까?')) {
-      try {
-        if (userId !== undefined) {
-          api.updateReview(
-            userId,
-            reviewId,
-            reviewContent,
-            reviewScore,
-          );
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        router.refresh();
-      }
-    }
-  };
-  const deleteReview = (reviewId: number): void => {
-    if (window.confirm('리뷰를 제거하시겠습니까?')) {
-      try {
-        if (userId !== undefined) {
-          api.deleteReview(userId, reviewId);
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        router.refresh();
-      }
+  const fetchReviews = async (id: number) => {
+    const res = await api.getReviewList(id);
+    if (res?.data?.myReviewList) {
+      setItemData(res.data.myReviewList);
     }
   };
 
@@ -78,82 +36,11 @@ const Review = (): React.ReactElement => {
         <div id="item-list">
           {itemData.length !== 0 ? (
             itemData.map(
-              (item: Review, index: number): React.ReactElement => (
-                <div
-                  id="item"
-                  className="flex justify-start w-full my-4 min-h-28"
-                  key={index}
-                >
-                  <div className="flex flex-col w-48">
-                    <div id="item-img" className="mask mask-square">
-                      <Image
-                        src={item.nutrientImageUrl}
-                        alt="item-img"
-                        width={100}
-                        height={100}
-                        onClick={() =>
-                          router.push(
-                            `/pilldetail/pill-detail?id=${item.nutrientId}`,
-                          )
-                        }
-                      />
-                    </div>
-                    <div
-                      id="item-info"
-                      className="flex flex-col items-center"
-                    >
-                      <span
-                        id="manufacturer"
-                        className="text-xs font-bold text-[#909090]"
-                      >
-                        {item.nutrientBrand}
-                      </span>
-                      <span
-                        id="name"
-                        className="text-sm font-extrabold text-[#3A3A3A]"
-                      >
-                        {item.nutrientName}
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col justify-around w-full m-2">
-                    <div id="review" className="flex flex-col">
-                      <span
-                        id="review-content"
-                        className="text-base font-base text-[#3A3A3A]"
-                      >
-                        {item.reviewContent}
-                      </span>
-                      <StarRating rating={item.reviewScore} />
-                    </div>
-                    <div
-                      id="buttons"
-                      className="flex justify-end space-x-1"
-                    >
-                      <button
-                        id="update-btn"
-                        className="btn btn-sm border-none text-[#909090] btn-outline"
-                        onClick={() =>
-                          updateReview(
-                            item.reviewId,
-                            item.reviewContent,
-                            item.reviewScore,
-                          )
-                        }
-                      >
-                        수정
-                      </button>
-                      <button
-                        id="delete-btn"
-                        className="btn btn-sm border-none text-[#909090] btn-outline"
-                        onClick={() => deleteReview(item.reviewId)}
-                      >
-                        삭제
-                      </button>
-                    </div>
-                  </div>
-                </div>
+              (item: Review): React.ReactElement => (
+                <MyReviewItem
+                  item={item}
+                  fetchReviews={fetchReviews}
+                />
               ),
             )
           ) : (
--- frontend/containers/MyPage/MyReviewItem.tsx
+++ frontend/containers/MyPage/MyReviewItem.tsx
@@ -1,114 +1,200 @@
+import { useEffect, useState } from 'react';
 import api from '@/apis/config';
-import Image from 'next/image';
-import React from 'react';
-
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { AxiosResponse } from 'axios';
+
 import StarRating from '@/components/common/StarRating';
 
 const MyReviewItem = (props: {
-  item: Item;
-  id: number;
+  item: Review;
+  fetchReviews: (id: number) => void;
 }): React.ReactElement => {
   const router = useRouter();
+  const [wantReview, setWantReview] = useState<boolean>();
+  const [userId, setUserId] = useState<number>();
 
-  // const updateReview = () => {
-  //   if (window.confirm('리뷰를 수정하시겠습니까?')) {
-  //     try {
-  //       api.updateReview(
-  //         userId,
-  //         reviewId,
-  //         reviewContent,
-  //         reviewScore,
-  //       );
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       router.refresh();
-  //     }
-  //   }
-  // };
-  // const deleteReview = () => {
-  //   if (window.confirm('리뷰를 제거하시겠습니까?')) {
-  //     try {
-  //       api.deleteReview(userId, reviewId);
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       router.refresh();
-  //     }
-  //   }
-  // };
+  const [rating, setRating] = useState<number>(5);
+  const [reviewContent, setReviewContent] = useState<string>('');
+
+  useEffect(() => {
+    setUserId(
+      Number.parseInt(localStorage.getItem('userId') as string),
+    );
+  }, []);
+
+  const updateReview = (
+    reviewId: number,
+    reviewContent: string,
+    reviewScore: number,
+  ) => {
+    if (userId !== undefined && reviewContent !== '') {
+      api
+        .updateReview(userId, reviewId, reviewContent, reviewScore)
+        .then(() => {
+          props.fetchReviews(userId);
+        });
+    }
+
+    setWantReview(false);
+  };
+
+  const deleteReview = (reviewId: number) => {
+    if (
+      window.confirm('리뷰를 제거하시겠습니까?') &&
+      userId !== undefined
+    ) {
+      api.deleteReview(userId, reviewId).then(() => {
+        props.fetchReviews(userId);
+      });
+    }
+  };
+
+  const handleStarClick = (
+    e: React.MouseEvent<HTMLInputElement, MouseEvent>,
+  ) => {
+    const target = e.target as HTMLInputElement;
+    setRating(Number(target.value));
+  };
+
+  const setReviewValue = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setReviewContent(e.target.value);
+  };
 
   return (
-    <div className="indicator">
-      <div
-        id="item"
-        className="flex justify-start w-full my-4 min-h-28"
-      >
-        <div className="flex flex-col w-48">
-          <div id="item-img" className="mask mask-square">
-            <Image
-              src={props.item.nutrientImageUrl}
-              alt="item-img"
-              width={100}
-              height={100}
+    <div
+      id="item"
+      className="flex justify-start items-center w-full my-4 min-h-28"
+    >
+      <div className="flex flex-col items-center w-32">
+        <div id="item-img" className="mask mask-square">
+          <Image
+            src={props.item.nutrientImageUrl}
+            alt="item-img"
+            width={100}
+            height={100}
+            onClick={() =>
+              router.push(
+                `/pilldetail/pill-detail?id=${props.item.nutrientId}`,
+              )
+            }
+          />
+        </div>
+        <div
+          id="item-info"
+          className="flex flex-col items-center whitespace-pre-wrap px-4"
+        >
+          <span
+            id="manufacturer"
+            className="text-xs font-bold text-[#909090]"
+          >
+            {props.item.nutrientBrand}
+          </span>
+          <span
+            id="name"
+            className="text-sm font-extrabold text-[#3A3A3A]"
+          >
+            {props.item.nutrientName}
+          </span>
+        </div>
+      </div>
+
+      {wantReview ? (
+        <div className="w-64 m-2 p-2">
+          <div className="grid grid-cols-2">
+            <div className="cols-span-1">별점을 등록해주세요</div>
+            <div className="rating cols-span-1">
+              <input
+                type="radio"
+                name="rating-2"
+                className="w-10 bg-orange-400 mask mask-star-2"
+                value="1"
+                onClick={handleStarClick}
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="w-10 bg-orange-400 mask mask-star-2"
+                value="2"
+                onClick={handleStarClick}
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="w-10 bg-orange-400 mask mask-star-2"
+                value="3"
+                onClick={handleStarClick}
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="w-10 bg-orange-400 mask mask-star-2"
+                value="4"
+                onClick={handleStarClick}
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="w-10 bg-orange-400 mask mask-star-2"
+                value="5"
+                onClick={handleStarClick}
+              />
+            </div>
+          </div>
+          <div className="pt-2">
+            <textarea
+              className="w-full textarea textarea-primary"
+              placeholder="리뷰를 작성해주세요"
+              onChange={setReviewValue}
+            ></textarea>
+          </div>
+          <div className="flex justify-end mb-0">
+            <button
+              id="update-btn"
+              className="btn btn-sm border-none text-[#909090] btn-outline"
               onClick={() =>
-                router.push(
-                  `/pilldetail/pill-detail?id=${props.item.nutrientId}`,
+                updateReview(
+                  props.item.reviewId,
+                  reviewContent,
+                  rating,
                 )
               }
-            />
-          </div>
-          <div id="item-info" className="flex flex-col items-center">
-            <span
-              id="manufacturer"
-              className="text-xs font-bold text-[#909090]"
             >
-              {props.item.nutrientBrand}
-            </span>
-            <span
-              id="name"
-              className="text-sm font-extrabold text-[#3A3A3A]"
-            >
-              {props.item.nutrientName}
-            </span>
+              수정
+            </button>
           </div>
         </div>
-
-        <div className="flex flex-col justify-around w-full m-2">
+      ) : (
+        <div className="flex flex-col justify-around w-64 m-2">
           <div id="review" className="flex flex-col">
             <span
               id="review-content"
               className="text-base font-base text-[#3A3A3A]"
             >
-              {'리뷰 내용은 1000자 이상으로 남겨주세요'}
+              {props.item.reviewContent}
             </span>
-            <StarRating
-              rating={
-                props.item.rating !== undefined
-                  ? props.item.rating
-                  : 0
-              }
-            />
+            <StarRating rating={props.item.reviewScore} />
           </div>
-          <div id="buttons" className="flex justify-end space-x-2">
+          <div id="buttons" className="flex justify-end space-x-1">
             <button
               id="update-btn"
               className="btn btn-sm border-none text-[#909090] btn-outline"
-              // onClick={() => updateReview()}
+              onClick={() => setWantReview(true)}
             >
               수정
             </button>
             <button
               id="delete-btn"
               className="btn btn-sm border-none text-[#909090] btn-outline"
-              // onClick={() => deleteReview()}
+              onClick={() => deleteReview(props.item.reviewId)}
             >
               삭제
             </button>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
