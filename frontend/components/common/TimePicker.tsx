229cacc05cd6446e01f8fa5987b73afa46da7d42
한상원 <swhan0428@gmail.com>
2023-05-18 02:40:12 +0900

    [S08P31A703-322] FE/Feat: Add feature
    - 알람 시간이 10분도 안남았을 때는 알람이 불가능하게 설정

--- frontend/components/common/TimePicker.tsx
+++ frontend/components/common/TimePicker.tsx
@@ -59,22 +59,42 @@ const times = [
 
 export default function TimePicker(props: { onClose: () => void }) {
   const [selected, setSelected] = useState(times[0]);
+  const hour = new Date().getHours();
+  const minute = new Date().getMinutes();
 
   const handleSubmit = () => {
-    api.addAlarm(selected.name);
-    console.log(selected.name);
+    const newDate1 = new Date(
+      2022,
+      1,
+      30,
+      parseInt(selected.name.split(':')[0]),
+      parseInt(selected.name.split(':')[1]),
+      1,
+    );
+
+    const newDate2 = new Date(2022, 1, 30, hour, minute, 1);
+
+    const timeDiff =
+      (newDate1.getTime() - newDate2.getTime()) / 1000 / 60;
+
+    if (timeDiff < -10 || timeDiff > 10) api.addAlarm(selected.name);
+    else
+      alert(
+        '알람 시간이 너무 가깝습니다. 다른 시간대를 선택해 주세요',
+      );
+    // console.log(selected.name);
     props.onClose();
   };
   return (
     <div className="flex items-baseline space-x-2">
-      <div className="top-16 w-48">
+      <div className="w-48 top-16">
         <Listbox value={selected} onChange={setSelected}>
           <div className="relative mt-1">
-            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
               <span className="block truncate">{selected.name}</span>
-              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                 <ChevronUpDownIcon
-                  className="h-5 w-5 text-gray-400"
+                  className="w-5 h-5 text-gray-400"
                   aria-hidden="true"
                 />
               </span>
@@ -85,7 +105,7 @@ export default function TimePicker(props: { onClose: () => void }) {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {times.map((time, timeIdx) => (
                   <Listbox.Option
                     key={timeIdx}
@@ -110,7 +130,7 @@ export default function TimePicker(props: { onClose: () => void }) {
                         {selected ? (
                           <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
                             <CheckIcon
-                              className="h-5 w-5"
+                              className="w-5 h-5"
                               aria-hidden="true"
                             />
                           </span>
