16ecdd79cee1aebd02a7a7a856e7daeff6ce91c8
한상원 <swhan0428@gmail.com>
2023-05-18 17:13:27 +0900

    [S08P31A703-322] FE/Feat: add slider main

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,12 +3,10 @@ import AppLayout from '@/components/layout/AppLayout';
 import HeaderLayout from '@/components/layout/HeaderLayout';
 import EntireList from '@/containers/PillListPage/EntireList';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import SimpleImageSlider from 'react-simple-image-slider';
 
 const bgImgSet = [
-  {
-    url: 'https://images.unsplash.com/photo-1628771065518-0d82f1938462?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-  },
   {
     url: 'https://images.unsplash.com/photo-1612540943977-98ce54bea8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
   },
@@ -20,31 +18,58 @@ const bgImgSet = [
   },
 ];
 
+// viewport 너비에 따라 다른 값을 반환하는 custom hook
+const useResponsiveDimension = () => {
+  const [dimension, setDimension] = useState({
+    width: '512px',
+    height: '300px',
+  });
+
+  useEffect(() => {
+    const updateDimension = () => {
+      if (window.innerWidth < 640) {
+        setDimension({ width: '360px', height: '250px' });
+      } else {
+        setDimension({ width: '512px', height: '300px' }); // 원하는 크기로 설정해주세요.
+      }
+    };
+
+    updateDimension();
+    window.addEventListener('resize', updateDimension);
+
+    return () =>
+      window.removeEventListener('resize', updateDimension);
+  }, []);
+
+  return dimension;
+};
+
 const Home: NextPageWithLayout = (): React.ReactElement => {
   const router = useRouter();
+  const { width, height } = useResponsiveDimension();
   return (
     <div>
-      <SimpleImageSlider
-        width="512px"
-        height="300px"
-        images={bgImgSet}
-        showNavs={false}
-        showBullets={true}
-        autoPlay={true}
-      />
-
-      {/* <div
+      <div
         className="relative hero"
         onClick={() => router.push('/survey')}
       >
-        <img src="https://images.unsplash.com/photo-1628771065518-0d82f1938462?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+        <SimpleImageSlider
+          width={width}
+          height={height}
+          images={bgImgSet}
+          showNavs={false}
+          showBullets={true}
+          autoPlay={true}
+          onClick={() => router.push('/survey')}
+        />
+
         <div className="flex flex-col items-baseline pt-40 sm:pt-56 pr-48 hero-content text-[#FFFFFF] w-[360px] sm:w-[512px]">
           <span className="text-lg font-bold whitespace-nowrap">
             나에게 맞는 영양제를 먹고 있는 걸까?
           </span>
           <span className="text-sm font-bold">설문하러 가기</span>
         </div>
-      </div> */}
+      </div>
       <EntireList />
     </div>
   );
